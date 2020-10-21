355743e90e6d94bda16aac16521c7d209f5c1796
irubenboy <rubenmoreno.saladillo@gmail.com>
2020-10-21 13:00:47 +0200

    Cambio de icono
    Se ha creado e implementado la función para cambiar los iconos.

--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
@@ -2,27 +2,69 @@ package es.iessaladillo.pedrojoya.exchange
 
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ImageView
-import android.widget.RadioButton
+import android.widget.EditText
 import android.widget.RadioGroup
-import androidx.annotation.DrawableRes
-import androidx.annotation.IdRes
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.widget.doAfterTextChanged
+import androidx.core.content.ContextCompat
 import es.iessaladillo.pedrojoya.exchange.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var lblAmount: TextView
     private lateinit var b: MainActivityBinding
+    private lateinit var inputAmount: EditText
+    private lateinit var rdgFromCoins: RadioGroup
+    private lateinit var fromIcon: TextView
+    private lateinit var rdgToCoins: RadioGroup
+    private lateinit var toIcon: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = MainActivityBinding.inflate(layoutInflater)
         setContentView(b.root)
-        // TODO
+        setupViews()
+    }
+
+    private fun setupViews() {
+        lblAmount = b.lblAmount
+        inputAmount = b.inputAmount
+        rdgFromCoins = b.rdgFromCoins
+        fromIcon = b.fromIcon
+        rdgToCoins = b.rdgToCoins
+        toIcon = b.toIcon
+        setInitialState(lblAmount)
+        inputAmount.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            changeTextViewColorOnFocus(lblAmount, hasFocus)
+        }
+        rdgFromCoins.setOnCheckedChangeListener { _, checkedId ->
+            fromIcon.setCompoundDrawablesWithIntrinsicBounds(changeIcon(checkedId), 0, 0, 0)}
+        rdgToCoins.setOnCheckedChangeListener { _, checkedId ->
+            toIcon.setCompoundDrawablesWithIntrinsicBounds(changeIcon(checkedId), 0, 0, 0)}
+    }
+
+    private fun changeIcon(checkedId: Int): Int {
+        var icon = 0
+
+        if(checkedId == b.fromDollar.id || checkedId == b.toDollar.id){
+            icon = R.drawable.ic_dollar
+        } else if (checkedId == b.fromEuro.id || checkedId == b.toEuro.id){
+            icon = R.drawable.ic_euro
+        } else if (checkedId == b.fromPound.id || checkedId == b.toPound.id) icon = R.drawable.ic_pound
+
+        return icon
+    }
+
+    private fun changeTextViewColorOnFocus(v : TextView, hasFocus: Boolean){
+       if (hasFocus) {
+           v.setTextColor(ContextCompat.getColor(this, R.color.pink_200))
+       } else {
+           setInitialState(v)
+       }
+    }
+
+    private fun setInitialState(v: TextView) {
+        v.setTextColor(ContextCompat.getColor(this, R.color.black))
     }
 
 }
